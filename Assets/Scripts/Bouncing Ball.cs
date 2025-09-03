8b08a96cd8377a6af5b4c9fc5ba13749de2700b1
Milan Dupuis <milan.dupuis@gmail.com>
2025-09-03 17:09:25 +0200

    [MODIFY] Ball bounce a fix number of time

--- Assets/Scripts/Bouncing Ball.cs
+++ Assets/Scripts/Bouncing Ball.cs
@@ -4,13 +4,15 @@ public class BouncingBall : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private float _damage;
+    [SerializeField] private int _maxNBounce;
+    private int _nBounce = 0;
     private Rigidbody2D _rb;
     private Vector3 lastVelocity;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _rb.AddForce(new Vector2(9.8f * _speed, 9.8f * _speed));
+        _rb.AddForce(new Vector2(_speed, _speed));
     }
 
     void Update()
@@ -20,11 +22,16 @@ public class BouncingBall : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (_nBounce > _maxNBounce)
+            Destroy(gameObject);
+
         if (collision.gameObject.tag == "Player")
         {
             collision.gameObject.GetComponent<PlayerHealth>().OnTakeDamage(_damage);
             Destroy(gameObject);
         }
+
+        _nBounce++;
 
         var direction = Vector2.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
         _rb.linearVelocity = direction * _speed;
